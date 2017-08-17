f0c833617101ad6195ea39890c56f6b082085eff
minerva22 <minerva.moawad@gmail.com>
2017-08-17 14:40:43 +0300

    display the superids in home page

--- cdradmin/models.py
+++ cdradmin/models.py
@@ -2,24 +2,31 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Superid (models.Model):
+class Superid(models.Model):
      
      superid = models.IntegerField(default=0)
      name = models.CharField(max_length=255)
+     
+     def __str__(self):
+        return self.name
 
-class Loan (models.Model):
+
+class Loan(models.Model):
 
      Losuperid = models.ForeignKey(Superid)
      Loshortdescription = models.CharField(max_length=255)
      LoDescription = models.CharField(max_length=600)
 
+
+     def __str__(self):
+        return self.Loshortdescription
+
 class Liability(models.Model):
     
-    Lisuperid = models.ForeignKey(Superid)
-    Lishortdescription = models.CharField(max_length=255)
-    Lidescription = models.CharField(max_length=600)
+      Lisuperid = models.ForeignKey(Superid)
+      Lishortdescription = models.CharField(max_length=255)
+      Lidescription = models.CharField(max_length=600)
 
-
-
-
+      def __str__(self):
+        return self.Lishortdescription
 
--- cdradmin/views.py
+++ cdradmin/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.views import generic
+from .models import Superid, Liability, Loan
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the cdr admin index.")
+class IndexView( generic.ListView):
+    template_name = 'cdradmin/index.html'
+    context_object_name = 'superid_list'
+    model= Superid
+    
+
+    def get_queryset(self):
+        """Return the  location by order alphabetic."""
+        return Superid.objects.order_by('superid')
+   
+
