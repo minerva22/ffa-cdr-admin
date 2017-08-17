310f2bb73165a143ddbe30d2bb1032ecb0a0897d
minerva22 <minerva.moawad@gmail.com>
2017-08-17 14:33:26 +0300

    create admin interface

--- cdradmin/admin.py
+++ cdradmin/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Superid, Loan, Liability
 
-# Register your models here.
+
+
+
+class SuperidAdmin(admin.ModelAdmin):  
+  list_display = ['superid']
+  
+  
+class LoanAdmin(admin.ModelAdmin):  
+  list_display = ['Loshortdescription']
+  
+  
+class LiabilityAdmin(admin.ModelAdmin):  
+  list_display = ['Lishortdescription']
+  
+    
+
+
+
+admin.site.register(Superid,SuperidAdmin)
+
+admin.site.register(Liability,LiabilityAdmin)
+admin.site.register(Loan,LoanAdmin)
